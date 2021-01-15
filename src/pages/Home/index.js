e29d0ae8a09c44f08c581a30eaaa637676933a61
Filipe Moreno <y8.filipe@gmail.com>
2021-01-15 07:46:15 +0000

    Update date

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,13 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import '../../global.css';
-import './styles.css';
-import api from '../../services/api';
-import {format} from 'date-fns'
+import React, { useEffect, useMemo, useState } from 'react'
+import '../../global.css'
+import './styles.css'
+import api from '../../services/api'
+import { format } from 'date-fns'
 import pt from 'date-fns/locale/pt-BR'
 // import api2 from '../../services/api2';
 
 const Home = () => {
-
-  const [dados, setDados] = useState([]);
-  const [obitos, setobitos] = useState('');
+  const [dados, setDados] = useState('')
 
   const [date, setDate] = useState(new Date())
   const dateFormatted = useMemo(
@@ -29,65 +27,65 @@ const Home = () => {
 
   return (
     <div>
-      {dados.map(dado => {
-            return (
-              <div className="update" key="objectId">
-              <h1>COVID Brasil</h1>
-              <p>Atualizado em: {dateFormatted}</p>
-            </div>
-            <div id="content">
-              <div className="card" id="confirmados">
-                <div className="header">
-                  <p>CONFIRMADOS</p>
-                </div>
-                <div className="container">
-                  <h4>{dado.confirmados.total}</h4>
-                  <p>Total</p>
-                  <br/>
-                  <h4>{dado.confirmados.recuperados}</h4>
-                  <p>Recuperados</p>
-                  <br/>
-                  <h4>{dado.confirmados.acompanhamento}</h4>
-                  <p>Em acompanhamento</p>
-                </div>
-                <div className="footer">
-                  <p>ÚLTIMAS 24 HORAS</p>
-                </div>
-                <div className="container" id="new">
-                  <h4>{dado.confirmados.novos}</h4>
-                  <p>Novos casos</p>
-                </div>
-              </div>
-              <br/>
-              <div className="card" id="obitos">
-                <div className="header">
-                  <p>ÓBITOS</p>
-                </div>
-                <div className="container">
-                  <h4>{dado.obitos.total}</h4>
-                  <p>Total</p>
-                  <br/>
-                  <h4>{dado.obitos.letalidade}%</h4>
-                  <p>Letalidade</p>
-                </div>
-                <div className="footer">
-                  <p>ÚLTIMAS 24 HORAS</p>
-                </div>
-                <div className="container" id="new">
-                  <h4>{dado.obitos.novos}</h4>
-                  <p>Novos óbitos</p>
-                </div>
-              </div>
-            </div>
-            )
-          })}
+      <div className="update">
+        <h1>COVID Brasil</h1>
+        <p>Atualizado em: {dateFormatted}</p>
+      </div>
+      <div id="content">
+        <div className="card" id="confirmados">
+          <div className="header">
+            <p>CONFIRMADOS</p>
+          </div>
+          <div className="container">
+            <h4>{dados.confirmados.total}</h4>
+            <p>Total</p>
+            <br />
+            <h4>{dados.confirmados.recuperados}</h4>
+            <p>Recuperados</p>
+            <br />
+            <h4>{dados.confirmados.acompanhamento}</h4>
+            <p>Em acompanhamento</p>
+          </div>
+          <div className="footer">
+            <p>ÚLTIMAS 24 HORAS</p>
+          </div>
+          <div className="container" id="new">
+            <h4>{dados.confirmados.novos}</h4>
+            <p>Novos casos</p>
+          </div>
+        </div>
+        <br />
+        <div className="card" id="obitos">
+          <div className="header">
+            <p>ÓBITOS</p>
+          </div>
+          <div className="container">
+            <h4>{dados.obitos.total}</h4>
+            <p>Total</p>
+            <br />
+            <h4>{dados.obitos.letalidade}%</h4>
+            <p>Letalidade</p>
+          </div>
+          <div className="footer">
+            <p>ÚLTIMAS 24 HORAS</p>
+          </div>
+          <div className="container" id="new">
+            <h4>{dados.obitos.novos}</h4>
+            <p>Novos óbitos</p>
+          </div>
+        </div>
+      </div>
       <div className="credit">
-        <p>Fonte: <a href="https://covid.saude.gov.br/">Ministério da Saúde</a></p>
-        <br/>
-        <p>© <a href="https://github.com/FilipeMoreno">Filipe Moreno</a></p>
+        <p>
+          Fonte: <a href="https://covid.saude.gov.br/">Ministério da Saúde</a>
+        </p>
+        <br />
+        <p>
+          © <a href="https://github.com/FilipeMoreno">Filipe Moreno</a>
+        </p>
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
